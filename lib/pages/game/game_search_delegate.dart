ff3e81b3eb3b4b22f264d17f449c6b08c9cd1bb7
段 駿 <dansyun1990@gmail.com>
2021-12-25 15:56:43 +0900

    Add:ゲーム検索レイアウト

---
+++ lib/pages/game/game_search_delegate.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class GameSearchDelegate extends SearchDelegate {
+  List<String> searchTerms = ['Apple', 'Google', 'Microsoft'];
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      )
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, null);
+      },
+      icon: const Icon(Icons.arrow_back),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var i in searchTerms) {
+      if (i.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(i);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var i in searchTerms) {
+      if (i.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(i);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+    );
+  }
+}
