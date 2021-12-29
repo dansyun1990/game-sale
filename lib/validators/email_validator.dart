3363841e12d04b5b994301aa3ee85e162a42a86b
段 駿 <dansyun1990@gmail.com>
2021-12-29 18:35:28 +0900

    Update:バリデータコメント追記

--- lib/validators/email_validator.dart
+++ lib/validators/email_validator.dart
@@ -1,4 +1,6 @@
+/// eメール入力チェック用バリデータ
 class EmailValidator {
+  /// eメールの形式チェック
   static bool validate(String value) {
     return RegExp(
             r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
--- lib/validators/password_validator.dart
+++ lib/validators/password_validator.dart
@@ -1,20 +1,26 @@
+/// パスワード入力チェック用バリデータ
 class PasswordValidator {
+  /// 1つの英大文字を含むチェック
   static bool validateUppercase(String value) {
     return RegExp(r'^(?=.*[A-Z])').hasMatch(value);
   }
 
+  /// 1つの英小文字を含むチェック
   static bool validateLowercase(String value) {
     return RegExp(r'^(?=.*[a-z])').hasMatch(value);
   }
 
+  /// 1つの数字を含むチェック
   static bool validateDigit(String value) {
     return RegExp(r'^(?=.*?[0-9])').hasMatch(value);
   }
 
+  /// 1つの記号を含むチェック
   static bool validateSpecialCharacter(String value) {
     return RegExp(r'^(?=.*?[!@#\$&*~])').hasMatch(value);
   }
 
+  /// 長さは8文字以上チェック
   static bool validateLength(String value) {
     return RegExp(r'^.{8,}$').hasMatch(value);
   }
