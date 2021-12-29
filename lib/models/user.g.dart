00691ffb85a7a5d7576f94c8212b958c43c44dff
段 駿 <syun.dan@kds-net.co.jp>
2021-12-29 20:55:39 +0900

    Add:認証用モデル追加

---
+++ lib/models/user.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_User _$$_UserFromJson(Map<String, dynamic> json) => _$_User(
+      id: json['id'] as String?,
+      email: json['email'] as String,
+      userName: json['userName'] as String,
+      favorites:
+          (json['favorites'] as List<dynamic>).map((e) => e as String).toList(),
+    );
+
+Map<String, dynamic> _$$_UserToJson(_$_User instance) => <String, dynamic>{
+      'id': instance.id,
+      'email': instance.email,
+      'userName': instance.userName,
+      'favorites': instance.favorites,
+    };
