d24cfd16f3164023624c7a4bfbbccbb683bd7212
段 駿 <syun.dan@kds-net.co.jp>
2021-12-29 18:45:51 +0900

    Update:ローカルストレージクラスコメント修正

--- lib/utils/local_storage.dart
+++ lib/utils/local_storage.dart
@@ -2,42 +2,42 @@ import 'dart:convert';
 
 import 'package:shared_preferences/shared_preferences.dart';
 
-/// SharedPreferencesシングルトンパータンのラッパークラスです。
+/// SharedPreferencesシングルトンパータンのラッパークラス
 class LocalStorage {
-  /// プライベートでスタティックのインスタンス作成します。
+  /// プライベートでスタティックのインスタンス作成
   static final _instance = LocalStorage._init();
 
-  /// ファクトリーコンストラクタでインスタンスを返します。
+  /// ファクトリーコンストラクタでインスタンスを返す
   factory LocalStorage() => _instance;
 
-  /// SharedPreferencesを遅延宣言します。
+  /// SharedPreferencesを遅延宣言
   static late SharedPreferences _sharedPreferences;
 
-  /// ネーミングコンストラクタでSharedPreferencesを初期化します。
+  /// ネーミングコンストラクタでSharedPreferencesを初期化
   LocalStorage._init() {
     _initStorage();
   }
 
-  /// 非同期でSharedPreferencesのインスタンスを取得します。
+  /// 非同期でSharedPreferencesのインスタンスを取得
   _initStorage() async {
     _sharedPreferences = await SharedPreferences.getInstance();
   }
 
-  /// [key]と[value]の形で値を保存します。
+  /// [key]と[value]の形で値を保存
   Future<void> setStorage(String key, dynamic value) async {
     await _initStorage();
     String type;
 
-    // valueの型を確認し、MapとListの場合、Jsonに変換して、文字列として保存します。
+    // valueの型を確認し、MapとListの場合、Jsonに変換して、文字列として保存
     if (value is Map || value is List) {
       type = 'String';
       value = const JsonEncoder().convert(value);
     }
-    // 以外の場合、valueの型の文字列を取得します。
+    // 以外の場合、valueの型の文字列を取得
     else {
       type = value.runtimeType.toString();
     }
-    // valueの型を基に、該当するメソッドで保存します。
+    // valueの型を基に、該当するメソッドで保存
     switch (type) {
       case 'String':
         _sharedPreferences.setString(key, value);
@@ -54,29 +54,29 @@ class LocalStorage {
     }
   }
 
-  /// [key]で保存した値を取得します。
+  /// [key]で保存した値を取得
   Future<dynamic> getStorage(String key) async {
     await _initStorage();
     dynamic value = _sharedPreferences.get(key);
 
-    // valueを確認し、Json文字列の場合、デコーダーします。
+    // valueを確認し、Json文字列の場合、デコーダー
     if (_isJson(value)) {
       return const JsonDecoder().convert(value);
     }
-    // 以外の場合、そのまま返します。
+    // 以外の場合、そのまま返す
     else {
       return value;
     }
   }
 
-  /// [key]が存在するかどうかを確認します。
+  /// [key]が存在するかどうかを確認
   Future<bool> hasKey(String key) async {
     await _initStorage();
     return _sharedPreferences.containsKey(key);
   }
 
-  /// [key]で保存した値を削除します。
-  /// 存在した場合[key]を削除し、trueを返す、存在しない場合、falseを返します。
+  /// [key]で保存した値を削除
+  /// 存在した場合[key]を削除し、trueを返す、存在しない場合、falseを返す。
   Future<bool> removeStorage(String key) async {
     await _initStorage();
     if (await hasKey(key)) {
@@ -87,20 +87,20 @@ class LocalStorage {
     }
   }
 
-  /// 保存した値を全部削除します。
+  /// 保存した値を全部削除
   Future<bool> clear() async {
     await _initStorage();
     return _sharedPreferences.clear();
   }
 
-  /// 保存した全ての[key]の型を取得します。
+  /// 保存した全ての[key]の型を取得
   Future<Set<String>> getKeys() async {
     await _initStorage();
     return _sharedPreferences.getKeys();
   }
 
-  /// [value]はJson文字列かどうかを確認します。
-  /// Json文字列の場合、trueを返す、以外の場合、falseを返します。
+  /// [value]はJson文字列かどうかを確認
+  /// Json文字列の場合、trueを返す、以外の場合、falseを返す
   _isJson(dynamic value) {
     try {
       const JsonDecoder().convert(value);
